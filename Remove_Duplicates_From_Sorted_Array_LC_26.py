6ed6400a93c8f5439c47064739c2cc88dc81558a
harshithaswarup1 <harshithaswarup1996@gmail.com>
2024-02-13 10:50:57 +0530

    Practice Problems

--- Remove_Duplicates_From_Sorted_Array_LC_26.py
+++ Remove_Duplicates_From_Sorted_Array_LC_26.py
@@ -1,32 +1,23 @@
-
 class Solution(object):
     def removeDuplicates(self, nums):
-        unique_elements = list(set(nums))
-        len_unique_elements = len(unique_elements)
-        diff = len(nums) - len(unique_elements)
-        unique_elements.extend(diff  * '_')
-        print (list(set(nums)))
+        if not nums:
+            return 0
+        i = 0
+
+        # Iterate through the array
+        for j in range(1, len(nums)):
+            # If the current element is different from the previous one, if the elements are
+            #unique, inc the value of "i" by 1
+            if nums[j] != nums[i]:
+                # Move the pointer
+                i += 1
+                # Update the array in-place
+                nums[i] = nums[j]
+
+        # The length of the modified array is (i + 1)
+        return i + 1
 
 
 solution = Solution()
-result = solution.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+result = solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
 
-
-# 80
-
-class Solution(object):
-    def removeDuplicates(self, nums):
-        new_list = []
-        for i in nums:
-            if nums.count(i) <= 2:
-                new_list.append(i)
-        diff = list(set(nums) - set(new_list))
-        new_list.extend(diff *2)
-        new_list.sort()
-        sorted_new_list = len(new_list)
-        return sorted_new_list
-        
-solution = Solution()
-result = solution.removeDuplicates([0,0,1,1,1,1,2,3,3])
-        
-        
